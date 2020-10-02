05c727b0ff518d466276d48e413cbaaba9b87471
Andreas Pedersen <andreas1992@outlook.dk>
2020-10-02 14:57:16 +0200

    Back-end update

--- madspildApp/src/main/java/com/example/demo/Controller/MadspildController.java
+++ madspildApp/src/main/java/com/example/demo/Controller/MadspildController.java
@@ -2,19 +2,53 @@ package com.example.demo.Controller;
 
 
 import com.example.demo.Models.MadspildFact;
+import com.example.demo.Models.PersonFact;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MadspildController {
 
-//    MadspildFact fact = new MadspildFact(1000,100000,500);
+//    Madspild Udregner Object
     MadspildFact fact;
 
 
+//    Persons in Array
+    List<PersonFact> personArray = new ArrayList<>();
+
+
+//    ================================= ANDREAS =================================
+    String text             = " Primære fokus er softwareudvikling og systemintegrationer.\n" +
+        "                   Med mine kompetencer og erfaringer, kan jeg hjælpe dig med at finde den helt rigtige softwareløsning.\n" +
+        "\n" +
+        "                   Udgangspunkts i dine krav, analyserer,designer og programmerer jeg din unikke softwareløsning.\n" +
+        "\n" +
+        "                   <br><br><h11 class=\"titel\">Software</h11>\n" +
+        "                   <br>UI/UX - Design, Prototying, Usability testing, System design, Software arkiteturer, API og Servicedesign, Agil projektstyring med Scrum\n" +
+        "\n" +
+        "                   <br><br><h12 class=\"titel\">Programmering</h12>\n" +
+        "                   <br>Jeg har de rette kompetencer, og er ekspert når det drejer sig om programmering.\n" +
+        "\n" +
+        "                   <br><br>Windows brugerflader, Webbrugerflader, Mobile apps, Server applikationer, Windows og Web Services, Databaser og DBMS, Microsoft Office udvidelser, System integrationer.";
+
+    PersonFact andreasObj   = new PersonFact("Andreas", text, "test.jgp");
+
+
+    //    ================================= JEFFREY =================================
+    String jeffText         = "test";
+
+    PersonFact jeffreyObj   = new PersonFact("Jeffrey", "Test", "test.jgp");
+//    ================================= TOBIAS =================================
+    PersonFact tobiasObj    = new PersonFact("Tobias", "Test", "test.jgp");
+
+
+// ========================================== GET INDEX =====================================
     @GetMapping("/")
     public String index(Model MadSpildController){
 
@@ -40,10 +74,20 @@ public class MadspildController {
         return "index";
     }
 
+    // ========================================== GET OMOS =====================================
+
     @GetMapping("/omos")
-    public String omOs(Model madspildFactModel){
+    public String omOs(Model PersonFact){
+
+        personArray.add(andreasObj);
+        personArray.add(jeffreyObj);
+        personArray.add(tobiasObj);
+
+        PersonFact.addAttribute("person", personArray);
+
         return "omOs";
     }
+// ========================================== POST INDEX =====================================
 
     @PostMapping("/postMadspildFact")
     public String postMadspildFact(WebRequest dataFromForm){
